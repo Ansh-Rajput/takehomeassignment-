f3b057ca5685b45b082e3987b21f826f58b300b1
Ansh-Rajput <anshravikrish425317@gmail.com>
2025-07-17 10:10:28 +0530

    added snyk test screen shot

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,80 +1,10 @@
 "use client";
-import { useState, useEffect } from "react";
-import PromptCard from "./PromptCard";
 import Demo from "./Demo";
 
-const PromptCardList = ({ data, handleTagClick }) => {
-  return (
-    <div className="mt-16 prompt_layout">
-      {data.map((post) => {
-        return (
-          <PromptCard
-            key={post._id}
-            post={post}
-            handleTagClick={handleTagClick}
-          />
-        );
-      })}
-    </div>
-  );
-};
-
 const Feed = () => {
-  const [searchText, setSearchText] = useState("");
-  const [searchTimeout, setSearchTimeout] = useState(null);
-  const [searchedResults, setSearchedResults] = useState([]);
-  const [posts, setPosts] = useState([]);
-
-  const filterPrompts = (searchText) => {
-    const regex = new RegExp(searchText, "i");
-
-    return posts.filter(
-      (item) =>
-        regex.test(item.creator.username) ||
-        regex.test(item.tag) ||
-        regex.test(item.prompt)
-    );
-  };
-
-  const handleSearchChange = (e) => {
-    clearTimeout(searchTimeout);
-    setSearchText(e.target.value);
-
-    setSearchTimeout(
-      setTimeout(() => {
-        const searchResult = filterPrompts(e.target.value);
-        setSearchedResults(searchResult);
-      }, 500)
-    );
-  };
-
-  // useEffect(() => {
-  //   const fetchPosts = async () => {
-  //     const response = await fetch("/api/prompt");
-  //     const data = await response.json();
-  //     setPosts(data);
-  //   };
-
-  //   fetchPosts();
-  // }, []);
-
-  const handleTagClick = (tag) => {
-    setSearchText(tag);
-    console.log(tag);
-    const searchResult = filterPrompts(tag);
-    setSearchedResults(searchResult);
-  };
   return (
     <section className="feed">
       <Demo />
-      {/* {searchText ? (
-        <PromptCardList
-          data={searchedResults}
-          handleTagClick={handleTagClick}
-        />
-      ) : (
-        <PromptCardList data={posts} handleTagClick={handleTagClick} />
-      )} */}
     </section>
   );
 };
